6a5bbbf131a48a51aa8e292001c421d9cea0e541
Jafar Ismai Mohamed <121930953+Jfism@users.noreply.github.com>
2024-11-28 13:10:03 +0500

    first commit

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<div id='header'>
+
+
+<div id='logo'> 
+    <h2>
+        <a href='index.php'>Azaz's learning system </a>
+    </h2>
+
+</div>
+
+<div id ='link'>
+    <h3><a href="#">logout </a>  </h3>
+</div>
+</div>
+
